88759e7f5bfc51cd5c8c9dd8206adf40e4a514aa
legendarytmt1205 <tranminhtrieudn@gmail.com>
2019-12-27 16:45:36 +0700

    tranminhtrieu

--- Module02/Java_Language/src/vn/codegym/thuchanh/PT_BacNhat.java
+++ Module02/Java_Language/src/vn/codegym/thuchanh/PT_BacNhat.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class PT_BacNhat {
     public static void main(String[] args) {
-//        System.out.println("Chương trình giải phương trình bậc nhất dạng Ax + B = 0" +
-//                "\n Vui lòng nhập vào 2 hằng số để ra kết quả ");
+        System.out.println("Chương trình giải phương trình bậc nhất dạng Ax + B = 0" +
+                "\n Vui lòng nhập vào 2 hằng số để ra kết quả ");
         Scanner scanner = new Scanner(System.in);
         System.out.print("a: ");
         double a = scanner.nextDouble();
